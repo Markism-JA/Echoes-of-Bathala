bf9c0fbbc7bf059a02a0e68bce5aa3415b36db68
marky <markism1224amarille@gmail.com>
2025-12-30 06:45:28 +0800

    documented the DI for backend infra

--- backend/src/GameBackend.Infra/DependencyInjection.cs
+++ backend/src/GameBackend.Infra/DependencyInjection.cs
@@ -8,6 +8,13 @@ namespace GameBackend.Infra
 {
     public static class DependencyInjection
     {
+        /// <summary>
+        /// Handles the registration of  infrastructure services for the GameBackend
+        /// and repository implementations.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+        /// <param name="configuration">The application <see cref="IConfiguration"/> containing connection strings and settings.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/> with infrastructure services registered.</returns>
         public static IServiceCollection AddGameInfrastructure(
             this IServiceCollection services,
             IConfiguration configuration
@@ -16,7 +23,7 @@ namespace GameBackend.Infra
             var connectionString = configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<GameDbContext>(options => options.UseNpgsql(connectionString));
 
-            //Auth Tool
+            // Auth Tool
             services.AddScoped<IAccountRepository, AccountRepository>();
 
             return services;
