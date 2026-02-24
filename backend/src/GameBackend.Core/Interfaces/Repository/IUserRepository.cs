f77addc1e57c2a7a0cc2dcbec50eb3802942db8e
marky <markism1224amarille@gmail.com>
2026-02-24 11:57:30 +0800

    clean up

--- backend/src/GameBackend.Core/Interfaces/Repository/IUserRepository.cs
+++ backend/src/GameBackend.Core/Interfaces/Repository/IUserRepository.cs
@@ -4,24 +4,19 @@ namespace GameBackend.Core.Interfaces.Repository
 {
     public interface IUserRepository
     {
-        // -- Reads
         Task<User?> GetByIdAsync(Guid accountId);
         Task<User?> GetByEmailAsync(string email);
         Task<User?> GetByUserNameAsync(string username);
         Task<User?> GetByWalletAddressAsync(string walletAddress);
 
-        // Fast Checks
         Task<bool> IsEmailTakenAsync(string email);
         Task<bool> IsUserNameTakenAsync(string username);
         Task<bool> IsWalletLinkedAsync(string walletAddress);
 
-        // -- Create
         Task CreateUserAsync(User account);
 
-        // -- Update
         Task UpdateUserAsync(User account);
 
-        // -- Delete
         Task DeleteUserAsync(User account);
     }
 }
