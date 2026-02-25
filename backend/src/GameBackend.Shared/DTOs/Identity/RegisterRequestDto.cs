90f41686db97a50be7689c11ee2bce02d84d68f8
marky <markism1224amarille@gmail.com>
2026-02-25 23:34:53 +0800

    constraint the username to only letters, numbers and underscores

--- backend/src/GameBackend.Shared/DTOs/Identity/RegisterRequestDto.cs
+++ backend/src/GameBackend.Shared/DTOs/Identity/RegisterRequestDto.cs
@@ -3,7 +3,13 @@ using System.ComponentModel.DataAnnotations;
 namespace GameBackend.Shared.DTOs.Identity
 {
     public record RegisterRequestDto(
-        [Required] [StringLength(20, MinimumLength = 3)] string Username,
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        [RegularExpression(
+            @"^[a-zA-Z0-9_]+$",
+            ErrorMessage = "Only letters, numbers, and underscores are allowed."
+        )]
+            string Username,
         [Required] [EmailAddress] string Email,
         [Required]
         [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
