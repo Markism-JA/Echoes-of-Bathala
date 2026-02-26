03049619a3a5caf9ce62d4913cdc5e95ab9dc05a
marky <markism1224amarille@gmail.com>
2026-02-26 10:21:19 +0800

    fix: data annotation problem

--- backend/src/GameBackend.Shared/DTOs/Identity/RegisterRequestDto.cs
+++ backend/src/GameBackend.Shared/DTOs/Identity/RegisterRequestDto.cs
@@ -1,21 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace GameBackend.Shared.DTOs.Identity
+namespace GameBackend.Shared.DTOs.Identity;
+
+public record RegisterRequestDto
 {
-    public record RegisterRequestDto(
-        [Required]
-        [StringLength(20, MinimumLength = 3)]
-        [RegularExpression(
-            @"^[a-zA-Z0-9_]+$",
-            ErrorMessage = "Only letters, numbers, and underscores are allowed."
-        )]
-            string Username,
-        [Required] [EmailAddress] string Email,
-        [Required]
-        [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
-            string Password,
-        [Required]
-        [property: Compare("Password", ErrorMessage = "Password do not match.")]
-            string ConfirmPassword
-    );
+    [Required]
+    [StringLength(20, MinimumLength = 3)]
+    [RegularExpression(
+        @"^[a-zA-Z0-9_]+$",
+        ErrorMessage = "Only letters, numbers, and underscores are allowed."
+    )]
+    public string Username { get; init; } = default!;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; init; } = default!;
+
+    [Required]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters.")]
+    public string Password { get; init; } = default!;
+
+    [Required]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+    public string ConfirmPassword { get; init; } = default!;
 }
