b28f7b6e80d3a3bbcd56c3e9b0ab53940bd1c323
marky <markism1224amarille@gmail.com>
2026-03-04 15:09:57 +0800

    feat(api): map error response

---
+++ backend/src/Backend/Echoes.API/Controllers/ApiController.cs
@@ -0,0 +1,41 @@
+using Echoes.Shared.Network.Contract;
+using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Echoes.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class BaseApiController : ControllerBase
+{
+    protected IActionResult Problem(List<Error> errors)
+    {
+        var response = new ErrorResponse();
+
+        if (errors.All(e => e.Type == ErrorType.Validation))
+        {
+            foreach (var error in errors)
+            {
+                response.Errors.Add(error.Code, error.Description);
+            }
+            response.Code = "ValidationFailure";
+            response.Description = "One or more validation errors occurred.";
+
+            return BadRequest(response);
+        }
+
+        var firstError = errors[0];
+        response.Code = firstError.Code;
+        response.Description = firstError.Description;
+
+        var statusCode = firstError.Type switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+            _ => StatusCodes.Status500InternalServerError,
+        };
+
+        return StatusCode(statusCode, response);
+    }
+}
