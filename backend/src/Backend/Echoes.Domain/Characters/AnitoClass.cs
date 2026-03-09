4d7410043d66ce9c67391e0da3e4ffb298258cfd
marky <markism1224amarille@gmail.com>
2026-03-10 03:11:53 +0800

    define the class type system

---
+++ backend/src/Backend/Echoes.Domain/Characters/AnitoClass.cs
@@ -0,0 +1,12 @@
+namespace Echoes.Domain.Characters
+{
+    public enum AnitoClass
+    {
+        Mangangaso, // Hunter
+        Guwardiya, // Guardian
+        Lihim, // Duelist
+        Kaanib, // Summoner
+        Manggagamot, // Healer
+        Mangkukulam, // Elementalist
+    }
+}
